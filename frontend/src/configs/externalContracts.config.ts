f7920911cc87c160ceee3d973eaae4cc25ce5fa1
Adsyst <0xadsyst@gmail.com>
2022-08-22 07:48:11 -0600

    Update contract addresses for latest deployment

--- frontend/src/configs/externalContracts.config.ts
+++ frontend/src/configs/externalContracts.config.ts
@@ -5,15 +5,15 @@ export const externalContractsAddressMap: TExternalContractsAddressMap = {
     AggregatorV3InterfaceETH: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
     AggregatorV3InterfaceBTC: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
     AggregatorV3InterfaceMATIC: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
-    CaptureTheStream: '0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1',
+    CaptureTheStream: '0x7a2088a1bFc9d81c55368AE168C2C02570cB814F',
     MockDAI: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
   },
   [80001]: {
     AggregatorV3InterfaceETH: '0x0715A7794a1dc8e42615F059dD6e406A6594651A',
     AggregatorV3InterfaceBTC: '0x007A22900a3B98143368Bd5906f8E17e9867581b',
     AggregatorV3InterfaceMATIC: '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada',
-    CaptureTheStream: '0xf47E5F4Ee4bE7D88309E2a0dDc99f2ccf0C3eDB0',
-    MockDAI: '0xE81Fca457ba225C7D0921207f0b24444b9303944'
+    CaptureTheStream: '0xF877d5E1368E2c0D15eAFA3BB9E7853e95DFf6C2',
+    MockDAI: '0x9b8e31E409b1cef401AbF174A95e17d26fdB502e'
   },
   [0]: {
     AggregatorV3InterfaceETH: '',
